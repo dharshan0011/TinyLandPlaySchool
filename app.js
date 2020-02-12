bc2b9c50c1361c2d0f4f48a233b8663c0ec55649
Dharshan K <dharshan0011@gmail.com>
2020-02-12 18:45:52 +0530

    added Procfile

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post("/",function(req,res){
       });
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Listening to port 3000");
 });
